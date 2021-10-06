38f72a362ca669efcd1e963ed4882555ebc06d29
2oublePeace <vova.emiryan@yandex.ru>
2021-10-06 23:28:25 +0400

    Добавьте файлы проекта.

---
+++ OfficeVisualComponent/CheckedListBoxControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OfficeVisualComponent
+{
+	public partial class CheckedListBoxControl : UserControl
+	{
+		public object Items {
+			get
+			{
+				return checkedListBox.Items;
+			}
+			set 
+			{
+				checkedListBox.DataSource = value;
+			} 
+		} 
+
+		public CheckedListBoxControl()
+		{
+			InitializeComponent();
+			
+		}
+	}
+}
